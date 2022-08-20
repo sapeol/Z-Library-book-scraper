57b11b2fd585b77fbf3547c67b1fc2de24a93ff2
sapeol <sudeep.kuchara@protonmail.com>
2022-08-21 01:21:28 +0530

    added file type and size

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-import fs from 'fs'
-import jsdom  from "jsdom";
+import fs from "fs";
+import jsdom from "jsdom";
 const { JSDOM } = jsdom;
-var output = fs.readFileSync('dom.html','utf8')
+var output = fs.readFileSync("dom.html", "utf8");
 const dom = new JSDOM(output);
-dom.window.document.querySelectorAll('.bookRow h3 a').forEach(el => {
-    console.log(el.innerHTML, "BOOK_ID ", el.href)
+dom.window.document.querySelectorAll(".bookRow").forEach((el) => {
+  console.log(
+    el.querySelector("h3 a").innerHTML,
+    "type ",
+    el.querySelector(".property__file .property_value").innerHTML,
+    "BOOK_ID ",
+    el.querySelector("h3 a").href
+  );
 });
-
-
